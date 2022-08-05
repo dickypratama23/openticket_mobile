e077a2a1f5716cc7ff2d353285af3ff3a415d02f
dickypratama <dickypratama4792@gmail.com>
2022-08-05 14:04:06 +0700

    refactor login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xfff4e7d3),
+      backgroundColor: const Color(0xff4C3A51),
       body: SafeArea(
         child: Center(
           child: SingleChildScrollView(
@@ -22,12 +23,14 @@ class _LoginPageState extends State<LoginPage> {
                 Icon(
                   Icons.send_rounded,
                   size: 100,
+                  color: const Color(0xffE7AB79),
                 ),
                 SizedBox(height: 75),
                 Text(
                   "OPENTICKET",
                   style: GoogleFonts.bebasNeue(
-                      fontSize: 52
+                      fontSize: 52,
+                      color: const Color(0xffE7AB79),
                   )
                 ),
                 SizedBox(height: 50),
@@ -38,7 +41,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.grey[200],
-                      border: Border.all(color: Colors.white),
+                      border: Border.all(color: const Color(0xffE7AB79)),
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: Padding(
@@ -60,7 +63,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.grey[200],
-                      border: Border.all(color: Colors.white),
+                      border: Border.all(color: const Color(0xffE7AB79)),
                       borderRadius: BorderRadius.circular(12),
                     ),
                     child: Padding(
@@ -79,44 +82,26 @@ class _LoginPageState extends State<LoginPage> {
 
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: const Color(0xff0881a3),
+                  child: MaterialButton(
+                    padding: EdgeInsets.all(18),
+                    minWidth: double.infinity,
+                    onPressed: () {  },
+                    color: const Color(0xffB25068),
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Center(
-                      child: Text(
-                        "Log In",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                        ),
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+
                   ),
                 ),
                 SizedBox(height: 25),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "New to site?",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    Text(
-                      " Create Account",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ],
-                )
 
               ],
             ),
